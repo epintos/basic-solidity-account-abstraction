794113cbdca440109b4f2e50beb77e33d037ebb3
Esteban Pintos <esteban.pintos@wetransfer.com>
2025-02-06 16:15:41 +0100

    ZkSync implementation setup

---
+++ src/zksync/ZkMinimalAccount.sol
@@ -0,0 +1,51 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.28;
+
+import { IAccount } from "@foundry-era-contracts/src/system-contracts/contracts/interfaces/IAccount.sol";
+import { Transaction } from
+    "@foundry-era-contracts/src/system-contracts/contracts/libraries/MemoryTransactionHelper.sol";
+
+/**
+ * @title ZkMinimalAccount
+ * @author Esteban Pintos
+ */
+contract ZkMinimalAccount is IAccount {
+    function validateTransaction(
+        bytes32 _txHash,
+        bytes32 _suggestedSignedHash,
+        Transaction calldata _transaction
+    )
+        external
+        payable
+        returns (bytes4 magic)
+    { }
+
+    function executeTransaction(
+        bytes32 _txHash,
+        bytes32 _suggestedSignedHash,
+        Transaction calldata _transaction
+    )
+        external
+        payable
+    { }
+    function executeTransactionFromOutside(Transaction calldata _transaction) external payable { }
+
+    function payForTransaction(
+        bytes32 _txHash,
+        bytes32 _suggestedSignedHash,
+        Transaction calldata _transaction
+    )
+        external
+        payable
+    { }
+
+    function prepareForPaymaster(
+        bytes32 _txHash,
+        bytes32 _possibleSignedHash,
+        Transaction calldata _transaction
+    )
+        external
+        payable
+    { }
+}
