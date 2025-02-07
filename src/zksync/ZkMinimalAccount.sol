1bb5abd19520c6d0980a3ebf3fd236e066bb1480
Esteban Pintos <esteban.pintos@wetransfer.com>
2025-02-07 11:51:17 +0100

    Implements ZkSync validateTransaction

--- src/zksync/ZkMinimalAccount.sol
+++ src/zksync/ZkMinimalAccount.sol
@@ -2,39 +2,106 @@
 
 pragma solidity ^0.8.28;
 
-import { IAccount } from "@foundry-era-contracts/src/system-contracts/contracts/interfaces/IAccount.sol";
-import { Transaction } from
-    "@foundry-era-contracts/src/system-contracts/contracts/libraries/MemoryTransactionHelper.sol";
+import {
+    IAccount,
+    ACCOUNT_VALIDATION_SUCCESS_MAGIC
+} from "@foundry-era-contracts/src/system-contracts/contracts/interfaces/IAccount.sol";
+import {
+    Transaction,
+    MemoryTransactionHelper
+} from "@foundry-era-contracts/src/system-contracts/contracts/libraries/MemoryTransactionHelper.sol";
+import { SystemContractsCaller } from
+    "@foundry-era-contracts/src/system-contracts/contracts/libraries/SystemContractsCaller.sol";
+import {
+    NONCE_HOLDER_SYSTEM_CONTRACT,
+    BOOTLOADER_FORMAL_ADDRESS
+} from "@foundry-era-contracts/src/system-contracts/contracts/Constants.sol";
+import { INonceHolder } from "@foundry-era-contracts/src/system-contracts/contracts/interfaces/INonceHolder.sol";
+import { MessageHashUtils } from "@openzeppelin/contracts/utils/cryptography/MessageHashUtils.sol";
+import { ECDSA } from "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
+import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
 
 /**
  * @title ZkMinimalAccount
  * @author Esteban Pintos
  */
-contract ZkMinimalAccount is IAccount {
+contract ZkMinimalAccount is IAccount, Ownable {
+    using MemoryTransactionHelper for Transaction;
+
+    /// ERRORS
+    error ZkMinimalAccount__NotEnoughBalance();
+    error ZkMinimalAccount__NotFromBootLoader();
+
+    /// MODIFIERS
+    modifier requireFromBootLoader() {
+        if (msg.sender != BOOTLOADER_FORMAL_ADDRESS) {
+            revert ZkMinimalAccount__NotFromBootLoader();
+        }
+        _;
+    }
+
+    /// FUNCTIONS
+
+    // CONSTRUCTOR
+    constructor() Ownable(msg.sender) { }
+
+    // EXTERNAL FUNCTIONS
+
+    /**
+     * @notice Validates a transaction
+     * @notice Increases nonce by calling the NonceHolder system contract
+     * @notice Checks if the contract has enough balance to pay for the transaction
+     * @notice Checks if the transaction signature is valid
+     * @param _transaction The transaction to validate
+     */
     function validateTransaction(
-        bytes32 _txHash,
-        bytes32 _suggestedSignedHash,
-        Transaction calldata _transaction
+        bytes32, /*_txHash*/
+        bytes32, /*_suggestedSignedHash*/
+        Transaction memory _transaction
     )
         external
         payable
+        requireFromBootLoader
         returns (bytes4 magic)
-    { }
+    {
+        // Increases nonce by calling the NonceHolder system contract
+        SystemContractsCaller.systemCallWithPropagatedRevert(
+            uint32(gasleft()),
+            address(NONCE_HOLDER_SYSTEM_CONTRACT),
+            0,
+            abi.encodeCall(INonceHolder.incrementMinNonceIfEquals, (_transaction.nonce))
+        );
+
+        uint256 totalRequiredBalance = _transaction.totalRequiredBalance();
+        if (totalRequiredBalance > address(this).balance) {
+            revert ZkMinimalAccount__NotEnoughBalance();
+        }
+
+        bytes32 txHash = _transaction.encodeHash();
+        bytes32 convertedHash = MessageHashUtils.toEthSignedMessageHash(txHash);
+        address signer = ECDSA.recover(convertedHash, _transaction.signature);
+        bool isValidSigner = signer == owner();
+        if (isValidSigner) {
+            magic = ACCOUNT_VALIDATION_SUCCESS_MAGIC;
+        } else {
+            magic = bytes4(0);
+        }
+    }
 
     function executeTransaction(
         bytes32 _txHash,
         bytes32 _suggestedSignedHash,
-        Transaction calldata _transaction
+        Transaction memory _transaction
     )
         external
         payable
     { }
-    function executeTransactionFromOutside(Transaction calldata _transaction) external payable { }
+    function executeTransactionFromOutside(Transaction memory _transaction) external payable { }
 
     function payForTransaction(
         bytes32 _txHash,
         bytes32 _suggestedSignedHash,
-        Transaction calldata _transaction
+        Transaction memory _transaction
     )
         external
         payable
@@ -43,9 +110,11 @@ contract ZkMinimalAccount is IAccount {
     function prepareForPaymaster(
         bytes32 _txHash,
         bytes32 _possibleSignedHash,
-        Transaction calldata _transaction
+        Transaction memory _transaction
     )
         external
         payable
     { }
+
+    // INTERNAL FUNCTIONS
 }
